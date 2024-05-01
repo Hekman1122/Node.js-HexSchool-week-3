b180a2574846021df93d64bd0d0abab13c527ceb
Hekman <yayakinetic@gmail.com>
2024-05-01 22:55:46 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const posts = require("./routes/posts");
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static("public"));
+
+//mongoose connect
+//mongoose connect
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => {
+    console.log("成功連接資料庫");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.get("/", (req, res, next) => {
+  res.send("HexSchool week 3");
+});
+
+app.use("/posts", posts);
+
+//Error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("listening on port 3000...")
+);
---
+++ routes/posts.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../model/postSchema");
+
+//取得所有文章列表
+router.get("/", async (req, res, next) => {
+  try {
+    const posts = await Post.find({});
+    res.json({
+      message: "Get all articles successfully.",
+      data: posts,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+//新增文章
+router.post("/", async (req, res, next) => {
+  try {
+    const newPost = new Post(req.body);
+    const savePost = await newPost.save();
+    res.json({
+      message: "Post new article successfully.",
+      data: savePost,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+//刪除所有文章
+router.delete("/", async (req, res, next) => {
+  try {
+    await Post.deleteMany({});
+    res.json({
+      message: "Delete all articles successfully",
+      data: null,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+//刪除指定文章
+router.delete("/:id", async (req, res, next) => {
+  //檢查是否有該筆資料
+  const target = await Post.findById(req.params.id);
+  if (target) {
+    try {
+      const deletePost = await Post.findByIdAndDelete(req.params.id);
+      res.json({
+        message: "Delete article successfully.",
+        data: deletePost,
+      });
+    } catch (e) {
+      next(e);
+    }
+  } else {
+    res.status(404).json({
+      message: "No such article.",
+    });
+  }
+});
+
+//更新指定文章
+router.patch("/:id", async (req, res, next) => {
+  //檢查是否有該筆資料
+  const target = await Post.findById(req.params.id);
+  if (target) {
+    try {
+      const updatePost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      res.json({
+        message: "Update article successfully.",
+        data: updatePost,
+      });
+    } catch (e) {
+      next(e);
+    }
+  } else {
+    res.status(404).json({
+      message: "No such article.",
+    });
+  }
+});
+
+module.exports = router;
